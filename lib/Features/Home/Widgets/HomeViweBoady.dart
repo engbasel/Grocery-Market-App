be8e8424d28b43f575eb4a09a4a3bea83e14be63
Basel Embaby <engbasel2025@gmail.com>
2024-05-31 21:24:15 +0300

    "Removed GradientBackgroundScaffold and replaced with Scaffold and ListView, removed import for customBackground"

--- lib/Features/Home/Widgets/HomeViweBoady.dart
+++ lib/Features/Home/Widgets/HomeViweBoady.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:grocerymarket/Core/widgets/customBackground.dart';
 import 'package:grocerymarket/Features/Home/Widgets/CurrentLocation.dart';
 import 'package:grocerymarket/Features/Home/Widgets/CustomAppBar.dart';
 import 'package:grocerymarket/Features/Home/Widgets/SearchBar.dart';
@@ -13,32 +12,35 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GradientBackgroundScaffold(
-      children: [
-        // const SizedBox(height: 20), // Add some spacing from the top
-        CustomAppBar(
-          horizontalPadding: 20,
-          title: 'Grocery Plus',
-          icon: FontAwesomeIcons.bell,
-          onTap: () {
-            Navigator.push(
+    return Scaffold(
+      body: ListView(
+        children: [
+          // const SizedBox(height: 20), // Add some spacing from the top
+          CustomAppBar(
+            horizontalPadding: 20,
+            title: 'Grocery Plus',
+            icon: FontAwesomeIcons.bell,
+            onTap: () {
+              Navigator.push(
                 context,
                 DialogRoute(
                   context: context,
                   builder: (context) {
                     return const NotificationView();
                   },
-                ));
-          },
-        ),
-        const SizedBox(height: 10), // Add some spacing from the top
-        const CurrentLocation(),
-        const CustomSearchBar(), // Ensure you instantiate CustomSearchBar
-        // Expanded(child: ListViewHome()),
-        CustomGridView(items: homeItems),
+                ),
+              );
+            },
+          ),
+          const SizedBox(height: 10), // Add some spacing from the top
+          const CurrentLocation(),
+          const CustomSearchBar(), // Ensure you instantiate CustomSearchBar
+          // Expanded(child: ListViewHome()),
+          CustomGridView(items: homeItems),
 
-        // Add some spacing from the top
-      ],
+          // Add some spacing from the top
+        ],
+      ),
     );
   }
 }
