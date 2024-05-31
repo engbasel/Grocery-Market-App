9465d0bf78de7f5ed54e7b8983fae330183199a7
Basel Embaby <engbasel2025@gmail.com>
2024-05-31 21:39:38 +0300

    "Removed GradientBackgroundScaffold and replaced with Scaffold and Container with LinearGradient decoration. Moved CustomAppBar and CustomGridView inside SafeArea and Column."

--- lib/Features/Snacks/Widgets/SnacksViewboady.dart
+++ lib/Features/Snacks/Widgets/SnacksViewboady.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:grocerymarket/Core/utils/Lists.dart';
 import 'package:grocerymarket/Core/widgets/CoustomGridViwe.dart';
 
-import '../../../Core/widgets/customBackground.dart';
 import '../../Home/Widgets/CustomAppBar.dart';
 
 class SnacksViewboady extends StatelessWidget {
@@ -10,20 +9,32 @@ class SnacksViewboady extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: GradientBackgroundScaffold(
-        children: [
-          CustomAppBar(
-            horizontalPadding: 0, verticalPadding: 0,
-            leadingFunction: () {},
-            title: 'Snacks',
-            leadingIcon: Icons.arrow_back,
-            // place: MainAxisAlignment.start,
-            // title: 'Snacks',
-            onTap: () {},
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0072B8), Color(0xFF00B4DB)],
           ),
-          CustomGridView(items: snackItems),
-        ],
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              CustomAppBar(
+                horizontalPadding: 0,
+                verticalPadding: 0,
+                leadingFunction: () {},
+                title: 'Snacks',
+                leadingIcon: Icons.arrow_back,
+                onTap: () {},
+              ),
+              Expanded(
+                child: CustomGridView(items: snackItems),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
