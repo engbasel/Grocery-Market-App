10309f43e23786daa1899f3207e99a4b9bb3be65
Basel Embaby <engbasel2025@gmail.com>
2024-05-31 21:33:11 +0300

    "Refactored FullPackageDetailsView: replaced GradientBackgroundScaffold with Scaffold, added Container with LinearGradient decoration, and rearranged widget layout."

--- lib/Features/OrdersAndHistory/Views/FullPackageDestailsView.dart
+++ lib/Features/OrdersAndHistory/Views/FullPackageDestailsView.dart
@@ -1,53 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:grocerymarket/Core/widgets/customBackground.dart';
 import 'package:grocerymarket/Features/Home/Widgets/CustomAppBar.dart';
 import 'package:grocerymarket/Features/OrdersAndHistory/Widgets/FullPackageDestailsProductItem.dart';
+import 'package:grocerymarket/Features/Wishlist/Views/Add to Wishlist.dart';
 
 import '../../../Core/widgets/CustomButton.dart';
-import '../../Wishlist/Views/Add to Wishlist.dart';
 
 class FullPackageDetailsView extends StatelessWidget {
   const FullPackageDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GradientBackgroundScaffold(
-      children: [
-        CustomAppBar(
-          leadingIcon: Icons.arrow_back,
-          leadingFunction: () {
-            Navigator.pop(context);
-          },
-          title: 'Full Package Destails',
-          onTap: () {},
-        ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 50),
-          child: Row(
-            children: [
-              Text('Products'),
-            ],
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0072B8), Color(0xFF00B4DB)],
           ),
         ),
-        const FullPackageDestailsProductItem(),
-        const FullPackageDestailsProductItem(),
-        const FullPackageDestailsProductItem(),
-        const SizedBox(height: 50),
-        CustomButton(
-          widthAtFirstOfButton: 55,
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) {
-                return const AddToWishlistView();
+        child: Column(
+          children: [
+            CustomAppBar(
+              leadingIcon: Icons.arrow_back,
+              leadingFunction: () {
+                Navigator.pop(context);
               },
-            ));
-          },
-          color: 0xff5EC401,
-          height: 50,
-          width: 250,
-          titleButton: 'Add New Product',
+              title: 'Full Package Details',
+              onTap: () {},
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Row(
+                children: [
+                  Text('Products'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: const [
+                  FullPackageDestailsProductItem(),
+                  FullPackageDestailsProductItem(),
+                  FullPackageDestailsProductItem(),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            CustomButton(
+              widthAtFirstOfButton: 55,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddToWishlistView(),
+                  ),
+                );
+              },
+              color: 0xff5EC401,
+              height: 50,
+              width: 250,
+              titleButton: 'Add New Product',
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
