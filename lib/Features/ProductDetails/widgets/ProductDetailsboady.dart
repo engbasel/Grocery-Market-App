5ee216e30a8be0444fafd88c614d13e701e8a6c9
Basel Embaby <engbasel2025@gmail.com>
2024-05-31 21:36:00 +0300

    "Refactored ProductDetailsBody widget: removed GradientBackgroundScaffold, updated layout and styling, and rearranged widgets."

--- lib/Features/ProductDetails/widgets/ProductDetailsboady.dart
+++ lib/Features/ProductDetails/widgets/ProductDetailsboady.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grocerymarket/Core/widgets/customBackground.dart';
 import 'package:grocerymarket/Features/Home/Widgets/CustomAppBar.dart';
 import 'package:grocerymarket/Core/widgets/CustomButton.dart';
 import 'package:grocerymarket/Features/ProductDetails/widgets/ListItemProductDetails.dart';
@@ -10,151 +9,164 @@ class ProductDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: GradientBackgroundScaffold(
-        children: [
-          CustomAppBar(
-            horizontalPadding: 0,
-            verticalPadding: 0,
-            title: "Product Details",
-            onTap: () {},
-            leadingFunction: () {},
-            leadingIcon: Icons.arrow_back_ios,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0072B8), Color(0xFF00B4DB)],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          GestureDetector(
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) {
-              //       return const WishlistDetailsView();
-              //     },
-              //   ),
-              // );
-            },
-            child: Image.asset(
-              'assets/ProductDetails/Milkimage.png',
-              height: 250,
-              width: 250,
-            ),
-          ),
-          const SizedBox(height: 25),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ProductImageListItem(
-                  imageLink: 'assets/ProductDetails/Milkimage.png',
+                CustomAppBar(
+                  horizontalPadding: 0,
+                  verticalPadding: 0,
+                  title: "Product Details",
                   onTap: () {},
-                  width: 100,
-                  height: 100,
+                  leadingFunction: () {},
+                  leadingIcon: Icons.arrow_back_ios,
                 ),
-                ProductImageListItem(
-                  imageLink: 'assets/ProductDetails/Milkimage.png',
-                  onTap: () {},
-                  width: 100,
-                  height: 100,
-                ),
-                ProductImageListItem(
-                  imageLink: 'assets/ProductDetails/Milkimage.png',
-                  onTap: () {},
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 15, right: 15),
-            child: Text(
-              'Arla DANO Full Cream Milk Powder Instant',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const Row(
-            children: [
-              SizedBox(width: 15),
-              Padding(
-                padding: EdgeInsets.only(right: 10), // Add padding here
-                child: Text(
-                  '1 KG',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 29),
-                ),
-              ),
-              SizedBox(width: 220),
-              Padding(
-                padding: EdgeInsets.only(left: 10), // Add padding here
-                child: Text(
-                  '৳182',
-                  style: TextStyle(
-                      color: Color(0xFF5EC401), fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.list),
-              ),
-              const Text('Dairy Products')
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 15),
-          ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.list),
-              ),
-              const Expanded(
-                child: Text(
-                  'Et quidem faciunt, ut summum bonum sit extremum et rationibus conquisitis de voluptate. Sed ut summum bonum sit id,',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    // Optional: Add more style properties as needed
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) {
+                    //       return const WishlistDetailsView();
+                    //     },
+                    //   ),
+                    // );
+                  },
+                  child: Image.asset(
+                    'assets/ProductDetails/Milkimage.png',
+                    height: 250,
+                    width: 250,
                   ),
-                  textAlign: TextAlign.start, // Optional: Adjust alignment
-                  softWrap: true, // Ensures text wraps to the next line
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 40),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'You can also check this items',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                const SizedBox(height: 25),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ProductImageListItem(
+                        imageLink: 'assets/ProductDetails/Milkimage.png',
+                        onTap: () {},
+                        width: 100,
+                        height: 100,
+                      ),
+                      ProductImageListItem(
+                        imageLink: 'assets/ProductDetails/Milkimage.png',
+                        onTap: () {},
+                        width: 100,
+                        height: 100,
+                      ),
+                      ProductImageListItem(
+                        imageLink: 'assets/ProductDetails/Milkimage.png',
+                        onTap: () {},
+                        width: 100,
+                        height: 100,
+                      ),
+                    ],
+                  ),
                 ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    'Arla DANO Full Cream Milk Powder Instant',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Row(
+                  children: [
+                    SizedBox(width: 15),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: Text(
+                        '1 KG',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 29),
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        '৳182',
+                        style: TextStyle(
+                          color: Color(0xFF5EC401),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.list),
+                    ),
+                    const Text('Dairy Products'),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.list),
+                      ),
+                      const Expanded(
+                        child: Text(
+                          'Et quidem faciunt, ut summum bonum sit extremum et rationibus conquisitis de voluptate. Sed ut summum bonum sit id,',
+                          style: TextStyle(fontSize: 16.0),
+                          textAlign: TextAlign.start,
+                          softWrap: true,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'You can also check this items',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
+                const ProductCardWidget(),
+                const ProductCardWidget(),
+                const ProductCardWidget(),
+                CustomButton(
+                  widthInFirstOfContainer: 0,
+                  widthBetweenItems: 0,
+                  color: 0xff5EC401,
+                  height: 50,
+                  iconButtonAtEnd: Icons.shop,
+                  onTap: () {},
+                  titleButton: 'Basel',
+                  width: 350,
+                ),
+                const SizedBox(height: 30),
               ],
             ),
           ),
-          const ProductCardWidget(),
-          const ProductCardWidget(),
-          const ProductCardWidget(),
-          CustomButton(
-            widthInFirstOfContainer: 0,
-            widthBetweenItems: 0,
-            color: 0xff5EC401,
-            height: 50,
-            iconButtonAtEnd: Icons.shop,
-            onTap: () {},
-            titleButton: 'Basel',
-            width: 350,
-          ),
-          const SizedBox(height: 30)
-        ],
+        ),
       ),
     );
   }
