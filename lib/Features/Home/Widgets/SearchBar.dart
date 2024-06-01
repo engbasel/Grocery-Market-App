fd8adf1a101bdbce1284673ab68b99b574a81677
Basel Embaby <engbasel2025@gmail.com>
2024-06-01 13:00:18 +0300

    "Updated SearchBar widget to include a search icon and navigation to SearchView"

--- lib/Features/Home/Widgets/SearchBar.dart
+++ lib/Features/Home/Widgets/SearchBar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:grocerymarket/Features/Search/Views/Search.dart';
 
 class CustomSearchBar extends StatelessWidget {
   const CustomSearchBar({super.key});
@@ -7,15 +9,39 @@ class CustomSearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: 'Search Anything',
-          filled: true,
-          fillColor: const Color(0xFFF0F1F2),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide.none,
-          ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF0F1F2),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search Anything',
+                  filled: true,
+                  fillColor: Colors.transparent,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: const FaIcon(FontAwesomeIcons.search),
+              onPressed: () {
+                // Implement the search action or navigation to the Search page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SearchView(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/Features/Search/Views/Search.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:grocerymarket/Features/Search/Widgets/SearchViewBoady.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SearchViewBoady();
+  }
+}
