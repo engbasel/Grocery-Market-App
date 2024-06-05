517d8fa020bf55a7761f8343df2ba26b864c26dd
Basel Embaby <engbasel2025@gmail.com>
2024-06-05 16:35:57 +0300

    "Removed commented out code and layout builder logic, replaced with a direct return of MoreInfoViewBodyDesktop()"

--- lib/Features/MoreInformation/Views/MoreInfoView.dart
+++ lib/Features/MoreInformation/Views/MoreInfoView.dart
@@ -1,41 +1,41 @@
-// import 'package:flutter/material.dart';
-// import 'package:grocerymarket/Features/MoreInformation/Widgets/MoreInfoViewBoady.dart';
-
-// class MoreInfoView extends StatelessWidget {
-//   const MoreInfoView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MoreInfoViewBodyDesktop();
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:grocerymarket/Features/MoreInformation/Views/MoreInfoViewBodyMobile.dart';
-import 'package:grocerymarket/Features/MoreInformation/Views/MoreInfoViewBodyTablet.dart';
-
-import '../Widgets/MoreInfoViewBoady.dart';
+import 'package:grocerymarket/Features/MoreInformation/Widgets/MoreInfoViewBoady.dart';
 
 class MoreInfoView extends StatelessWidget {
   const MoreInfoView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        print(constraints.maxWidth);
-
-        if (constraints.maxWidth >= 800) {
-          // Desktop layout
-          return const MoreInfoViewBodyDesktop();
-        } else if (constraints.maxWidth >= 452) {
-          // Tablet layout
-          return const MoreInfoViewBodyTablet();
-        } else {
-          // Mobile layout
-          return const MoreInfoViewBodyMobile();
-        }
-      },
-    );
+    return const MoreInfoViewBodyDesktop();
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:grocerymarket/Features/MoreInformation/Views/MoreInfoViewBodyMobile.dart';
+// import 'package:grocerymarket/Features/MoreInformation/Views/MoreInfoViewBodyTablet.dart';
+
+// import '../Widgets/MoreInfoViewBoady.dart';
+
+// class MoreInfoView extends StatelessWidget {
+//   const MoreInfoView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constraints) {
+//         print(constraints.maxWidth);
+
+//         if (constraints.maxWidth >= 800) {
+//           // Desktop layout
+//           return const MoreInfoViewBodyDesktop();
+//         } else if (constraints.maxWidth >= 452) {
+//           // Tablet layout
+//           return const MoreInfoViewBodyTablet();
+//         } else {
+//           // Mobile layout
+//           return const MoreInfoViewBodyMobile();
+//         }
+//       },
+//     );
+//   }
+// }
