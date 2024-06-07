db57971e9f570d6aec8b8f65d282e932b5adc80d
Basel Embaby <engbasel2025@gmail.com>
2024-06-08 00:12:06 +0300

    Hoso Changes

--- lib/Features/Auth/Signup/views/YourInformationView.dart
+++ lib/Features/Auth/Signup/views/YourInformationView.dart
@@ -1,101 +1,143 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:grocerymarket/Core/widgets/CoustomTextFormFildes/customTextFormField.dart';
-import 'package:grocerymarket/Core/widgets/CustomButton.dart';
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:grocerymarket/Core/widgets/CoustomTextFormFildes/customTextFormField.dart';
+// import 'package:grocerymarket/Core/widgets/CustomButton.dart';
 
-import '../../../Home/Widgets/CustomAppBar.dart';
+// import '../../../Home/Widgets/CustomAppBar.dart';
+
+// class YourInformationView extends StatelessWidget {
+//   final VoidCallback onTapNext;
+//   final PageController pageController;
+//   final TextEditingController textEditingController;
+
+//   const YourInformationView(
+//       {super.key,
+//       required this.onTapNext,
+//       required this.pageController,
+//       required this.textEditingController});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     double height = MediaQuery.of(context).size.height;
+//     double width = MediaQuery.of(context).size.width;
+//     return Scaffold(
+//       body: ListView(
+//         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//         children: [
+//           CustomAppBar(
+//             title: 'Your Information',
+//             onTap: () {},
+//             leadingFunction: () {
+//               Navigator.pop(context);
+//             },
+//             leadingIcon: Icons.arrow_back,
+//           ),
+//           Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 const Text(
+//                   'It looks like you don’t have an account with this number. Please provide some information for a secure service.',
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 CircleAvatar(
+//                   radius: 80,
+//                   backgroundColor: const Color(0xffe3e5e6),
+//                   child: IconButton(
+//                     onPressed: () {},
+//                     icon: const Icon(FontAwesomeIcons.camera),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 Padding(
+//                   padding: const EdgeInsets.symmetric(
+//                     horizontal: 15,
+//                     vertical: 5,
+//                   ),
+//                   child: CustomButton(
+//                     widthBetweenItems: 50,
+//                     widthAtFirstOfButton: width * 0.3,
+//                     height: 50,
+//                     color: 0xff236cd9,
+//                     titleButton: 'Sync From Facebook',
+//                     TextColor: Colors.white,
+//                     iconButtonAtEnd: FontAwesomeIcons.facebook,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 Padding(
+//                   padding:
+//                       const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+//                   child: customTextFormField(
+//                     controller: textEditingController,
+//                     labelText: 'Full name',
+//                     hintText: 'Full name',
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   height: height * 0.1,
+//                 ),
+//                 CustomButton(
+//                   TextColor: Colors.white,
+//                   width: width * 0.5,
+//                   widthBetweenItems: width * 0.1,
+//                   widthAtFirstOfButton: width * 0.1,
+//                   height: 50,
+//                   color: 0xff5ec401,
+//                   titleButton: 'Next',
+//                   iconButtonAtEnd: FontAwesomeIcons.arrowRight,
+//                   onTap: () {
+//                     pageController.nextPage(
+//                       duration: const Duration(milliseconds: 500),
+//                       curve: Curves.ease,
+//                     );
+//                   },
+//                 ),
+//                 SizedBox(
+//                   height: height * 0.1,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
 
 class YourInformationView extends StatelessWidget {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
   final VoidCallback onTapNext;
-  final PageController pageController;
 
-  const YourInformationView(
-      {super.key, required this.onTapNext, required this.pageController});
+  const YourInformationView({
+    super.key,
+    required this.emailController,
+    required this.passwordController,
+    required this.onTapNext,
+  });
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        children: [
-          CustomAppBar(
-            title: 'Your Information',
-            onTap: () {},
-            leadingFunction: () {
-              Navigator.pop(context);
-            },
-            leadingIcon: Icons.arrow_back,
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'It looks like you don’t have an account with this number. Please provide some information for a secure service.',
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                CircleAvatar(
-                  radius: 80,
-                  backgroundColor: const Color(0xffe3e5e6),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(FontAwesomeIcons.camera),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 5,
-                  ),
-                  child: CustomButton(
-                    widthBetweenItems: 50,
-                    widthAtFirstOfButton: width * 0.3,
-                    height: 50,
-                    color: 0xff236cd9,
-                    titleButton: 'Sync From Facebook',
-                    TextColor: Colors.white,
-                    iconButtonAtEnd: FontAwesomeIcons.facebook,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  child: customTextFormField(
-                    labelText: 'Full name',
-                    hintText: 'Full name',
-                  ),
-                ),
-                SizedBox(
-                  height: height * 0.1,
-                ),
-                CustomButton(
-                  TextColor: Colors.white,
-                  width: width * 0.5,
-                  widthBetweenItems: width * 0.1,
-                  widthAtFirstOfButton: width * 0.1,
-                  height: 50,
-                  color: 0xff5ec401,
-                  titleButton: 'Next',
-                  iconButtonAtEnd: FontAwesomeIcons.arrowRight,
-                  onTap: () {
-                    pageController.nextPage(
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                    );
-                  },
-                ),
-                SizedBox(
-                  height: height * 0.1,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        TextField(
+          controller: emailController,
+          decoration: const InputDecoration(labelText: 'Email'),
+        ),
+        TextField(
+          controller: passwordController,
+          decoration: const InputDecoration(labelText: 'Password'),
+          obscureText: true,
+        ),
+        ElevatedButton(
+          onPressed: onTapNext,
+          child: const Text('Next'),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,48 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:grocerymarket/Features/Auth/Login/Cubit/LoginCubit.dart';
+// import 'package:grocerymarket/Features/Snacks/views/Snacksview.dart';
+// import 'package:grocerymarket/Features/Splash/views/SplashScreenView.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//   await Hive.openBox('userBox'); // Open a box for storing user data
+//   runApp(const GroceryApp());
+// }
+
+// class GroceryApp extends StatelessWidget {
+//   const GroceryApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (context) => LoginCubit(), // Provide the LoginCubit
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'Grocery App',
+//         // home: LoginViewBody(), // Set LoginViewBody as the home
+//         home: const SplashScreenView(),
+//         // home: SignupView(),
+//         routes: {
+//           'SnacksView': (context) =>
+//               const SnacksView(), // Define route for SnacksView
+//         }, // Set LoginViewBody as the home
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grocerymarket/Features/Auth/Login/Cubit/LoginCubit.dart';
+import 'package:grocerymarket/Features/Auth/Signup/Cubit/SignupCubit.dart'; // Import the SignupCubit
 import 'package:grocerymarket/Features/Snacks/views/Snacksview.dart';
 import 'package:grocerymarket/Features/Splash/views/SplashScreenView.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+import 'Features/Auth/Signup/Cubit/SignupStatus.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,18 +56,21 @@ class GroceryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LoginCubit(), // Provide the LoginCubit
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginCubit>(
+            create: (context) => LoginCubit()), // Provide LoginCubit
+        BlocProvider<SignupCubit>(
+            create: (context) => SignupCubit()), // Provide SignupCubit
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Grocery App',
-        // home: LoginViewBody(), // Set LoginViewBody as the home
         home: const SplashScreenView(),
-        // home: SignupView(),
         routes: {
           'SnacksView': (context) =>
               const SnacksView(), // Define route for SnacksView
-        }, // Set LoginViewBody as the home
+        },
       ),
     );
   }
